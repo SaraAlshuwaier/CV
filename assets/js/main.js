44415b03f1f5be61aea6eb681e2e99b0fb2974cd
SaraAlshuwaier <saraalshuwaier11@gmail.com>
2024-05-16 20:59:07 +0300

    Update main.js

--- assets/js/main.js
+++ assets/js/main.js
@@ -42,6 +42,26 @@
     el.addEventListener('scroll', listener)
   }
   
+/**
+   * Navbar links active state on scroll
+   */
+let navbarlinks = select('#navbar .scrollto', true)
+const navbarlinksActive = () => {
+  let position = window.scrollY + 200
+  navbarlinks.forEach(navbarlink => {
+    if (!navbarlink.hash) return
+    let section = select(navbarlink.hash)
+    if (!section) return
+    if (position >= section.offsetTop && position <= (section.offsetTop + section.offsetHeight)) {
+      navbarlink.classList.add('active')
+    } else {
+      navbarlink.classList.remove('active')
+    }
+  })
+}
+window.addEventListener('load', navbarlinksActive)
+onscroll(document, navbarlinksActive)
+
   /**
    * Scrolls to an element with header offset
    */
@@ -91,6 +111,25 @@
     window.addEventListener('load', toggleBacktotop)
     onscroll(document, toggleBacktotop)
   }
+
+ /**
+   * Mobile nav toggle
+   */
+ on('click', '.mobile-nav-toggle', function(e) {
+  select('#navbar').classList.toggle('navbar-mobile')
+  this.classList.toggle('bi-list')
+  this.classList.toggle('bi-x')
+})
+
+/**
+ * Mobile nav dropdowns activate
+ */
+on('click', '.navbar .dropdown > a', function(e) {
+  if (select('#navbar').classList.contains('navbar-mobile')) {
+    e.preventDefault()
+    this.nextElementSibling.classList.toggle('dropdown-active')
+  }
+}, true)
 
   /**
    * Scrool with ofset on links with a class name .scrollto
